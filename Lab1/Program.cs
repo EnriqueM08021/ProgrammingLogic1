36279058af9c83ccbbe2cb1929bdedb03c5b2d4a
“EnriqueM08021” <S0953370@acad.tri.c.edu>
2025-03-18 14:32:53 -0400

    Lab1 Done

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,22 @@ using System;
   {
     static void Main(string[] args)
     {
-      // Step 1
-    }
+      // Step 1 Basic Output
+      Console.WriteLine("Enrique is in IT-1050")
+      // Step 2 Using Multi Line Comments
+      /* Enrique 
+      IT-1050 Lab 1 */
+      // Step 3 Declare and Use Variables
+      int myNum = 7;
+      Console.WriteLine("My Favorite Number is"+myNum+)
+      double ProgNum = 3.0;
+      Console.WriteLine("I have written"+ProgNum+"programs")
+      string FavLang = python;
+      Console.WriteLine("My favorite programming Language is"+FavLang+)
+      bool MyBool = Yes;
+      Console.WriteLine(+MyBool+"I do have programming experience")
+       const string School = Tri-C;
+         Console.WriteLine("I am currently attending"+School+)
+          }  
+
   }
-Console.WriteLine("Enrique is in IT-1050")
