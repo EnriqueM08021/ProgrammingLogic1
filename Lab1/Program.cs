ed8234c7f548c706a6cc0097a861342b8594634a
“EnriqueM08021” <S0953370@acad.tri.c.edu>
2025-03-18 13:56:50 -0400

    Lab1!

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,7 +1,10 @@
 using System;
-class Program
-{
- static void Main() {
-    Console.WriteLine("Hello, world!");
- }
-}
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Step 1
+    }
+  }
+Console.WriteLine("Enrique is in IT-1050")
