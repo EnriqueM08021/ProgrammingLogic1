ad70104e21437f3986441681d02f0d61d07ec308
EnriqueM08021 <S00953370@acad.tri-c.edu>
2025-04-14 20:45:47 +0000

    Lesson4 Done

---
+++ Lab4/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Part 1
+        for (int a = 0; a < 11; a++) 
+            {
+                Console.WriteLine(a);
+            }
+    //Part 2
+        for (int b = 0; b < 21; b = b +2)
+            {
+                Console.WriteLine(b);
+            }
+    //Part 3
+        int c =5;
+        while (c>0)
+        {
+            Console.WriteLine(c);
+            c--;
+        }
+    //Part 4
+        int d;
+        do
+        {
+            Console.Write("Enter a number greater than 100: ");
+            string input = Console.ReadLine();
+                if (int.TryParse(input, out d))
+            {
+                if (d > 100)
+                {
+                    Console.WriteLine("Correct Number: " + d);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Number must be bigger than 100.");
+                }
+            }
+        } while (true);
+    //Part 4
+    for (int e = 10; e < 1001; e = e +10)
+            {
+                Console.WriteLine(e);
+            }
+    //Part 5
+ for (int f = 1; f <= 10; f++)
+        {
+            for (int g = 1; g <= f; g++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+    }
+    }
+}
