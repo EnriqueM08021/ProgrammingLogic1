65718fa12ba0194dabc2a6454439d8bbd2f944fa
EnriqueM08021 <S00953370@acad.tri-c.edu>
2025-05-12 03:20:49 +0000

    Lab 8

---
+++ Lab8/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab8;
+
+using System;
+
+// Step 1
+class Animal
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+// Step 2
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow");
+    }
+}
+
+// Step 2
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 3: In Main()
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
+        myAnimal.animalSound();
+        myCat.animalSound();
+        myDog.animalSound();
+    }
+}
+
